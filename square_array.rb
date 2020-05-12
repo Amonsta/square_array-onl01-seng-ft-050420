64cba236467dc730b3b1d37cbeac3deae5b92ece
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-12 20:29:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,11 +2,18 @@
 
 
 def square_array(array)
-  numbers = [] 
+  numbers = []   
+  
+  #empty array that I used for my new squared numbers
+  
   array.each do |num|
-    numbers << num ** 2
+    numbers << num ** 2 
+    
+    # Need to square the numbers in my array while also adding them to a new array
   end
-  numbers
+  numbers  
+  
+  # this is my new array being returned so that I can see what was squared
 end
 
 
